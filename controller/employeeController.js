4896c27382e1c8c3ce1ef56da4799c28859e2f04
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-11-01 20:46:56 -0500

    updated package.json

--- controller/employeeController.js
+++
@@ -1,7 +0,0 @@
-export const fetch = async(req, res) => {
-    try {
-        res.json("Hello World")
-    } catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
-    }
-}
--- routes/employeeRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import {fetch} from "../controller/employeeController.js"
-const route = express.Router()
-
-route.get("/fetch", fetch)
-
-export default route
